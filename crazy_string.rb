e74eee2cf9f2a171d49fea06222fa68e5e037ca0
josephinem11 <josephine.man0112@yahoo.com>
2018-08-07 14:51:12 +0000

    automatically backed up by learn

--- crazy_string.rb
+++ crazy_string.rb
@@ -1,7 +1,6 @@
-def crazy_strings
-  (string_one, string_two)
+def crazy_strings(string_one, string_two)
   string_one = string_one.upcase.reverse
-  string two = string_two.swapcase.gsub "(s,z)"
+  string two = string_two.swapcase.gsub ("s", "z")
 end
 
 crazy_strings("Hello, friends")
